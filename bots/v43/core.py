ebddda36bce80c97786d26bb442d2a750f16d4b4
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-26 11:15:53 +0000

    mv

--- bots/v43/core.py
+++ bots/v43/core.py
@@ -8,13 +8,13 @@ class Core(Entity):
     def __init__(self, ct: Controller) -> None:
         super().__init__(ct)
         self.spawned = 0
-        self.max_builders = 2
 
     def run(self, ct: Controller) -> None:
         ti, _ = ct.get_global_resources()
         cost, _ = ct.get_builder_bot_cost()
         h_cost, _ = ct.get_harvester_cost()
-        if self.spawned >= self.max_builders:
+        max_builders = min(2, 2 + ct.get_current_round() // 50)
+        if self.spawned >= max_builders:
             return
         multiplier = 3.0 + self.spawned * 0.5
         if ti >= cost * multiplier + h_cost:
