56e848754927c9d760f700f5569e532aca6b3662
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-10 20:45:53 +0100

    e

---
+++ cambcpypy/src/cambcpypy/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "1.6.2"
+
+from cambcpypy.engine import (
+    Controller,
+    Direction,
+    EntityType,
+    Environment,
+    GameConstants,
+    GameError,
+    Position,
+    ResourceType,
+    Team,
+)
+
+__all__ = [
+    "Controller",
+    "Direction",
+    "EntityType",
+    "Environment",
+    "GameConstants",
+    "GameError",
+    "Position",
+    "ResourceType",
+    "Team",
+]
