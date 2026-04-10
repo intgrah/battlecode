237ba5818634aec07699e2454fa080c2c2b0ec33
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-10 22:34:49 +0100

    warn if not pypy

--- cambcpypy/src/cambcpypy/__init__.py
+++ cambcpypy/src/cambcpypy/__init__.py
@@ -1,5 +1,15 @@
 __version__ = "1.6.2"
 
+import sys
+import warnings
+
+if sys.implementation.name != "pypy":
+    warnings.warn(
+        f"cambcpypy is running under {sys.implementation.name}. Expected PyPy.",
+        RuntimeWarning,
+        stacklevel=1,
+    )
+
 from cambcpypy.engine import (
     Controller,
     Direction,
