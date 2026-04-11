11eebd06fdd9e7bfa945fb30d612a18729a760bc
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-11 12:12:11 +0100

    e

---
+++ bots/intgrah/v53.4.0/builder/extra.py
@@ -0,0 +1,33 @@
+from cambc import Controller, Environment
+
+from .state import State
+from .task_build_conveyors import route_to_core
+from .task_repair import is_dangling
+
+
+def fix_enemy_conveyor(state: State, ct: Controller) -> bool:
+    nearby_positions = ct.get_nearby_tiles(2)
+    for pos in nearby_positions:
+        if state.leads_to_enemy_building(pos) and ct.can_destroy(pos):
+            ct.destroy(pos)
+            if ct.can_build_road(pos):
+                ct.build_road(pos)
+                return True
+    return False
+
+
+def pave_near_harvesters(state: State, ct: Controller) -> bool:
+    nearby_positions = ct.get_nearby_tiles(2)
+    for pos in nearby_positions:
+        if (
+            pos in state.adjacent_to_harvester
+            and not state.get_building(pos)
+            and state.get_env(pos) != Environment.WALL
+        ):
+            if is_dangling(state, ct, pos):
+                route_to_core(state, ct, pos)
+                return True
+            if ct.can_build_road(pos):
+                ct.build_road(pos)
+                return True
+    return False
