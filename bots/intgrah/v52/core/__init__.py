015909725e3f0064983bfa41bb2b67fb606cb6ec
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-11 11:55:03 +0100

    v5x

--- bots/intgrah/v52/core/__init__.py
+++ bots/intgrah/v52/core/__init__.py
@@ -13,7 +13,7 @@ __all__ = ["Core"]
 class Core(StationaryUnit):
     INITIAL_SPAWNS = 6
     INCOME_SAMPLES = 16
-    MAX_BUILDERS = 15
+    MAX_BUILDERS = 12
     INCOME_HEADROOM = 5
     SURPLUS_BASELINE = 50
     SURPLUS_SCALE_FACTOR = 60
@@ -63,7 +63,7 @@ class Core(StationaryUnit):
         has_surplus = ti > Core.SURPLUS_BASELINE + Core.SURPLUS_SCALE_FACTOR * (
             ct.get_scale_percent() / 100
         )
-        return (rnd > 24 and has_income) or (rnd > 48 and has_surplus)
+        return (rnd > 20 and has_income) or (rnd > 40 and has_surplus)
 
     def try_spawn(self, ct: Controller) -> None:
         dir9 = [*DIR8, Direction.CENTRE]
