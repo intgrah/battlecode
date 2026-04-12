b14db91d458635238c8f5fa7afa2b5a30ee929fb
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-12 21:05:53 +0100

    add bellman ford even though it fucking sucks

---
+++ pkg/bench_nav/src/bench_nav/sssp/bellman_ford.py
@@ -0,0 +1,22 @@
+from bench_nav.common import INF
+
+
+def bellman_ford(
+    n: int, cost: list[int], pnb: list[list[int]], start: int
+) -> list[int]:
+    dist = [INF] * n
+    dist[start] = 0
+    for _ in range(n - 1):
+        changed = False
+        for node in range(n):
+            d = dist[node]
+            if d == INF:
+                continue
+            for nb in pnb[node]:
+                nd = d + cost[nb]
+                if nd < dist[nb]:
+                    dist[nb] = nd
+                    changed = True
+        if not changed:
+            break
+    return dist
