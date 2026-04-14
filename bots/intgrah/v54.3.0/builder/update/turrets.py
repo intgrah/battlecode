a93c118b03d3370fe54ce6ee0e931cd9834f8ba4
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-14 21:33:12 +0100

    self.in_bounds

--- bots/intgrah/v54.3.0/builder/update/turrets.py
+++ bots/intgrah/v54.3.0/builder/update/turrets.py
@@ -12,7 +12,6 @@ if TYPE_CHECKING:
 
 def update_ore_denial(self: Builder, ct: Controller) -> None:
     w = self.w
-    my_team = ct.get_team()
     self.deny_ore_neighbours = set()
     for pos in self.nearby_positions:
         env = self.env[pos.y * w + pos.x]
@@ -21,21 +20,21 @@ def update_ore_denial(self: Builder, ct: Controller) -> None:
         has_enemy = False
         for d in DIR8:
             n = pos.add(d)
-            if not (0 <= n.x < self.w and 0 <= n.y < self.h):
+            if not self.in_bounds(n):
                 continue
             nb = self.buildings[n.y * w + n.x]
-            if nb is not None and nb.team != my_team:
+            if nb is not None and nb.team != self.my_team:
                 has_enemy = True
                 break
             if ct.is_in_vision(n):
                 uid = ct.get_tile_builder_bot_id(n)
-                if uid is not None and ct.get_team(uid) != my_team:
+                if uid is not None and ct.get_team(uid) != self.my_team:
                     has_enemy = True
                     break
         if has_enemy:
             for d in DIR4:
                 n = pos.add(d)
-                if 0 <= n.x < self.w and 0 <= n.y < self.h:
+                if self.in_bounds(n):
                     self.deny_ore_neighbours.add(n)
 
 
@@ -46,21 +45,15 @@ def update_enemy_turrets(self: Builder, ct: Controller) -> None:
     if self.nearest_enemy_turret:
         i = self.nearest_enemy_turret.y * w + self.nearest_enemy_turret.x
         match self.buildings[i]:
-            case BuildingGunner(team=t) | BuildingSentinel(team=t) if (
-                t != ct.get_team()
-            ):
+            case BuildingGunner(team=t) | BuildingSentinel(team=t) if t != self.my_team:
                 pass
             case _:
                 self.nearest_enemy_turret = None
 
     min_dist = INF
     for pos in self.nearby_positions:
-        if not (0 <= pos.x < self.w and 0 <= pos.y < self.h):
-            continue
         match self.buildings[pos.y * w + pos.x]:
-            case BuildingGunner(team=t) | BuildingSentinel(team=t) if (
-                t != ct.get_team()
-            ):
+            case BuildingGunner(team=t) | BuildingSentinel(team=t) if t != self.my_team:
                 dist = (pos.x - my_pos.x) ** 2 + (pos.y - my_pos.y) ** 2
                 if dist < min_dist:
                     min_dist = dist
