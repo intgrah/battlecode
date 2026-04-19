0319200724c343c4afc1fa65a1b5713932db6e27
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-04-19 06:30:57 +0100

    v54.7.0

---
+++ bots/intgrah/v54.7.0/config.py
@@ -0,0 +1,6 @@
+import os
+from typing import Final
+
+DEBUG_RESIGN: Final[bool] = bool(os.getenv("DEBUG_RESIGN"))
+DEBUG_DUMP: Final[bool] = bool(os.getenv("DEBUG_DUMP"))
+DEBUG_TIMING: Final[bool] = bool(os.getenv("DEBUG_TIMING"))
