a78e13eb34ba4908a311cc8586c456fa82d8c98f
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-05-05 09:35:34 +0100

    e

--- bots/meme/meme/main.py
+++ bots/meme/meme/main.py
@@ -16,31 +16,27 @@ class Player:
         self._ti_ore: tuple[int, int] | None = None
         self._ax_ore: tuple[int, int] | None = None
         self._ti_path: list[tuple[int, int]] = []
-        try:
-            m = Map26.read()
-            if m.cores:
-                ref_x, ref_y = m.cores[0].x, m.cores[0].y
-                best_ti = best_ax = -1
-                for y in range(m.height):
-                    for x in range(m.width):
-                        env = m.tile(x, y)
-                        match env:
-                            case Environment.ORE_TITANIUM:
-                                d = (x - ref_x) ** 2 + (y - ref_y) ** 2
-                                if best_ti < 0 or d < best_ti:
-                                    best_ti = d
-                                    self._ti_ore = (x, y)
-                            case Environment.ORE_AXIONITE:
-                                d = (x - ref_x) ** 2 + (y - ref_y) ** 2
-                                if best_ax < 0 or d < best_ax:
-                                    best_ax = d
-                                    self._ax_ore = (x, y)
-                if self._ti_ore is not None:
-                    self._ti_path = astar.run(
-                        m, start=self._ti_ore, goal=(ref_x, ref_y)
-                    )
-        except Exception as e:  # noqa: BLE001
-            print(f"[map] {e}")
+        self.map = m = Map26.read()
+
+        if m.cores:
+            ref_x, ref_y = m.cores[0].x, m.cores[0].y
+            best_ti = best_ax = 1_000_000
+            for y in range(m.height):
+                for x in range(m.width):
+                    env = m.tile(x, y)
+                    match env:
+                        case Environment.ORE_TITANIUM:
+                            d = (x - ref_x) ** 2 + (y - ref_y) ** 2
+                            if d < best_ti:
+                                best_ti = d
+                                self._ti_ore = (x, y)
+                        case Environment.ORE_AXIONITE:
+                            d = (x - ref_x) ** 2 + (y - ref_y) ** 2
+                            if d < best_ax:
+                                best_ax = d
+                                self._ax_ore = (x, y)
+            if self._ti_ore is not None:
+                self._ti_path = astar.run(m, start=self._ti_ore, goal=(ref_x, ref_y))
 
     def run(self, ct: Controller) -> None:
         self.g = Game.open(RawMem(), ct)
