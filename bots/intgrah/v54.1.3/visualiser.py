29ab803c8ef43e7141a2c155691b77a121333890
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-05-07 07:46:56 +0100

    delete

--- bots/intgrah/v54.1.3/visualiser.py
+++
@@ -1,1 +0,0 @@
-../../../pkg/visualiser/src/visualiser/__init__.py
--- bots/intgrah/v54.2.4/config.py
+++
@@ -1,5 +0,0 @@
-import os
-from typing import Final
-
-DEBUG_DUMP: Final[bool] = bool(os.getenv("DEBUG_DUMP"))
-DEBUG_TIMING: Final[bool] = bool(os.getenv("DEBUG_TIMING"))
